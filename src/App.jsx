e9a3ff58ece9daf9c7cdf18475bcb535aee4e761
Malhar <71639096+mxm-1x@users.noreply.github.com>
2025-02-03 23:53:59 +0530

    new brach change

--- src/App.jsx
+++ src/App.jsx
@@ -97,20 +97,6 @@ const App = () => {
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        {/* {trendingMovies.length > 0 && (
-          <section className="trending">
-            <h2>Trending Movies</h2>
-
-            <ul>
-              {trendingMovies.map((movie, index) => (
-                <li key={movie.$id}>
-                  <p>{index + 1}</p>
-                  <img src={movie.poster_url} alt={movie.title} />
-                </li>
-              ))}
-            </ul>
-          </section>
-        )} */}
 
         <section className="all-movies">
           <h2>All Movies</h2>
